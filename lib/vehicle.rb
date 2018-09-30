32a15ecc6ab97e1606627c7dd21e9f9c22d5499a
Ashley Wong <ashwong04@gmail.com>
2018-09-30 19:17:55 +0000

    automatically backed up by learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -2,6 +2,12 @@ class Vehicle
   
   attr_accessor :wheel_size, :wheel_number
   
-  def initialize(wheel_size, sheel_)
+  def initialize(wheel_size, wheel_number)
+    @wheel_size= wheel_size
+    @wheel_number= wheel_number
+  end
+  
+  def go
+    "vrrrrr"
 
 end
